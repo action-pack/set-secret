fb93a9aaf43d87860ec06d134342c212b5eb34d8
Kroese <kroese@users.noreply.github.com>
2023-10-16 12:26:19 +0200

    style: Prettier

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
-const sodium = require('tweetsodium');
+const sodium = require("tweetsodium");
 
 const token = core.getInput("token");
 const octokit = github.getOctokit(token);
@@ -13,110 +13,97 @@ const repoName = context.payload.repository.name;
 const ownerName = context.payload.repository.owner.login;
 
 let repository = core.getInput("repository");
-if(repository === 'false'){
-  repository = repoName;
-}
+if (repository === "false") repository = repoName;
 
 let owner = core.getInput("owner");
-if(owner === 'false'){
-  owner = ownerName;
-}
+if (owner === "false") owner = ownerName;
 
-let push_to_org = (core.getInput("org") !== 'false');
+const push_to_org = core.getInput("org") !== "false";
 
 function get_() {
 
-  if(push_to_org) {
-    return '/orgs/' + owner;
-  }
-  else {
-    if(repository.includes("/"))
-    {
-       return '/repos/' + repository;
-    }
-    return '/repos/' + owner + '/' + repository;
-  }
+  if (push_to_org) return "/orgs/" + owner;
+  if (repository.includes("/")) return "/repos/" + repository;
+
+  return "/repos/" + owner + "/" + repository;
+
 }
 
-const getPublicKey = async() => {
+const getPublicKey = async () => {
 
-  let url = "GET "
-  url += get_()
-  url += "/actions/secrets/public-key"
+  let url = "GET ";
+  url += get_();
+  url += "/actions/secrets/public-key";
 
-  let { data } = await octokit.request(url)
+  let { data } = await octokit.request(url);
 
   return data;
-}
+};
 
-const createSecret = async(key_id, key, secret) => {
+const createSecret = async (key_id, key, secret) => {
 
-  const messageBytes = Buffer.from(secret)
-  const keyBytes = Buffer.from(key, 'base64')
-  const encryptedBytes = sodium.seal(messageBytes, keyBytes)
+  const messageBytes = Buffer.from(secret);
+  const keyBytes = Buffer.from(key, "base64");
+  const encryptedBytes = sodium.seal(messageBytes, keyBytes);
 
   return {
-    encrypted_value: Buffer.from(encryptedBytes).toString('base64'),
-    key_id
-  }
-
-}
+    encrypted_value: Buffer.from(encryptedBytes).toString("base64"),
+    key_id,
+  };
+};
 
 const setSecret = (data) => {
 
-  let url = 'PUT '
-  url += get_()
-  url += '/actions/secrets/' + name
+  let url = "PUT ";
+  url += get_();
+  url += "/actions/secrets/" + name;
 
   return octokit.request(url, {
-    data
-  })
-
-}
+    data,
+  });
+};
 
 const bootstrap = async () => {
-  
+
   try {
-    
-    const {key_id, key} = await getPublicKey()
+  
+    const { key_id, key } = await getPublicKey();
 
-    let data = await createSecret(key_id, key, value)
+    let data = await createSecret(key_id, key, value);
 
-    if(push_to_org){
-      data['visibility'] = 'all'
+    if (push_to_org) data["visibility"] = "all";
+
+    const response = await setSecret(data);
+
+    if (response.status === 201) {
+      return "Succesfully created secret " + name;
     }
 
-    const response = await setSecret(data)
-
-    if(response.status === 201) {
-      return "Succesfully created secret " + name
+    if (response.status === 204) {
+      return "Succesfully updated secret " + name;
     }
-    
-    if(response.status === 204) {
-      return "Succesfully updated secret " + name
-    }
-    
-    throw new Error("ERROR: Wrong status was returned: " + response.status)
 
-  }catch (e) {
+    throw new Error("ERROR: Wrong status was returned: " + response.status);
+    
+  } catch (e) {
     core.setFailed(get_() + ": " + e.message);
-    console.error(e)
+    console.error(e);
   }
-}
+};
 
 bootstrap()
   .then(
-    result => {
+    (result) => {
       // eslint-disable-next-line no-console
-      if(result != null) {
+      if (result != null) {
         console.log(result);
       }
     },
-    err => {
+    (err) => {
       // eslint-disable-next-line no-console
       core.setFailed(err.message);
-      console.error(err)
-    }
+      console.error(err);
+    },
   )
   .then(() => {
     process.exit();
